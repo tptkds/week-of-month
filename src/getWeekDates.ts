20a04a6e370ac42452fc4d6f82d0e019077816e4
KimYouGyoung <tptkds12@gmail.com>
2024-10-11 00:24:37 +0900

    feat:getWeekDates

---
+++ src/getWeekDates.ts
@@ -0,0 +1,31 @@
+import getWeekOfMonth from "./getWeekOfMonth";
+
+const getWeekDates = (year: number, month: number, weekOfMonth: number) => {
+  const days = [];
+
+  let curDate = new Date();
+  for (let i = 0; i < 5; i++) {
+    curDate = new Date(year, month - 1, i === 0 ? 1 : i * 7);
+    const {
+      year: cYear,
+      month: cMonth,
+      weekOfMonth: cWeek,
+    } = getWeekOfMonth(curDate);
+    if (year === cYear && month === cMonth && weekOfMonth === cWeek) break;
+  }
+
+  let curDay = curDate?.getDay();
+  if (curDay === 0) curDay = 7;
+  const startDate = new Date(year, month - 1, curDate.getDate() - curDay + 1);
+  for (let i = 0; i < 7; i++) {
+    days.push(
+      new Date(
+        startDate.getFullYear(),
+        startDate.getMonth(),
+        startDate.getDate() + i
+      )
+    );
+  }
+  return days;
+};
+export default getWeekDates;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { WeekOfMonth } from "./type";
 import { default as getWeekOfMonth } from "./getWeekOfMonth";
 import { default as getNextWeekOfMonth } from "./getNextWeekOfMonth";
 import { default as getPrevWeekOfMonth } from "./getPrevWeekOfMonth";
-
-export { getWeekOfMonth, getNextWeekOfMonth, getPrevWeekOfMonth };
+import { default as getWeekDates } from "./getWeekDates";
+export { getWeekOfMonth, getNextWeekOfMonth, getPrevWeekOfMonth, getWeekDates };
 export type { WeekOfMonth };
